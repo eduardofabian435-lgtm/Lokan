b7637a7f460c22d4372ca22600bca9e13b66f9b8
eduardofabian435-lgtm <seu-email-do-github@gmail.com>
2026-05-28 16:45:49 -0300

    atualizar base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/Lokan/',
+    base: '/sistemas-lokan.com.br/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
