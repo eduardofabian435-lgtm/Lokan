de8aa38cff34638144ec2e93b6b830d994a4600b
eduardofabian435-lgtm <seu-email-do-github@gmail.com>
2026-05-28 16:17:29 -0300

    atualizar base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/Cursos-bantu/',
+    base: '/Lokan/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
